2d89823e7525f334fa05f6d5b86c7023d230bd19
Lorant Domokos <domokos.lorant@yahoo.com>
2021-03-22 23:10:24 -0700

    Current user impl

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-   res.send("Hi there!");
+   const token = req.session?.jwt;
+
+   if (!token) {
+      return res.send({ currentUser: null });
+   }
+
+   try {
+      const payload = jwt.verify(token, process.env.JWT_KEY!);
+      res.send({ currentUser: payload });
+   } catch (error) {
+      res.send({ currentUser: null });
+   }
 });
 
 export { router as currentUserRouter };
