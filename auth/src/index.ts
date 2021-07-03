0cc79569ae99a8fb5064cc9bdf57172c65649b88
Lorant Domokos <domokos.lorant@yahoo.com>
2021-07-03 10:56:11 -0700

    undo

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,8 +4,6 @@ import { app } from "./app";
 // Express has trouble with async function directly,
 // so safe to const and the run.
 const start = async () => {
-  console.log("Starting up...");
-
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
